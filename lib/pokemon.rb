a5aed5550a24bb99377fdf15a9bd3dad3805944b
Ryan Manchester <ryanm17@gmail.com>
2018-04-21 17:10:32 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,8 +1,11 @@
 class Pokemon
+  attr_accessor :name, :type, :db
 
-def initialize(name:, type:, db:)
-
-end
+  def initialize(name:, type:, db:)
+    @name = name
+    @type = type
+    @db = db
+  end
 
 
 end
